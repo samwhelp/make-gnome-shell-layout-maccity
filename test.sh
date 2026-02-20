496b587de533b1852c032ff6e40bcca388daa36e
samwhelp <samwhelp@users.noreply.github.com>
2026-02-20 12:47:25 +0800

    fix

--- test.sh
+++ test.sh
@@ -5,3 +5,20 @@ DEFAULT_MASTER_DISTRO="debian"
 REF_MASTER_DISTRO="${REF_MASTER_DISTRO:=$DEFAULT_MASTER_DISTRO}"
 
 echo "REF_MASTER_DISTRO=${REF_MASTER_DISTRO}"
+
+
+##
+## ## Test
+##
+## run
+##
+## ``` sh
+## bash <(curl -L https://raw.githubusercontent.com/samwhelp/make-gnome-shell-layout-maccity/refs/heads/main/test.sh)
+## ```
+##
+## run
+##
+## ``` sh
+## REF_MASTER_DISTRO=ubuntu bash <(curl -L https://raw.githubusercontent.com/samwhelp/make-gnome-shell-layout-maccity/refs/heads/main/test.sh)
+## ```
+##
